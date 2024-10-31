85f57d5720e525fabbd00a9438dd7707237671c8
EnsslinAdrian <ensslin.adrian@web.de>
2024-10-31 16:11:46 +0100

    check

--- src/app/main-content/chat/chat-header/chat-header.component.ts
+++ src/app/main-content/chat/chat-header/chat-header.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-chat-header',
   standalone: true,
   imports: [
     MatIconModule,
+    FormsModule,
+    MatButtonModule,
   ],
   templateUrl: './chat-header.component.html',
   styleUrl: './chat-header.component.scss'
 })
 export class ChatHeaderComponent {
 
+
+  openDialog() {
+
+  }
 }
