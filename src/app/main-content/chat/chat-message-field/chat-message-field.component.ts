0f77a43b5d9f70a96a21da1e62aef8b9de81cf1f
EnsslinAdrian <ensslin.adrian@web.de>
2024-10-31 18:49:19 +0100

    create emojis

--- src/app/main-content/chat/chat-message-field/chat-message-field.component.ts
+++ src/app/main-content/chat/chat-message-field/chat-message-field.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-chat-message-field',
   standalone: true,
   imports: [
     MatIcon,
+    PickerComponent,
+    CommonModule,
   ],
   templateUrl: './chat-message-field.component.html',
   styleUrl: './chat-message-field.component.scss'
 })
 export class ChatMessageFieldComponent {
+  openEmojis: boolean = false;
 
+  toggleEmojis(event: Event): void {
+    event.stopPropagation(); 
+    this.openEmojis = !this.openEmojis;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeEmojisOnOutsideClick(event: Event): void {
+    if (this.openEmojis && !(event.target as HTMLElement).closest('.emojis-container')) {
+      this.openEmojis = false;
+    }
+  }
 }
