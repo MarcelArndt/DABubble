d66201b8c0a0714a9a2e349fd226f66b434d2509
EnsslinAdrian <ensslin.adrian@web.de>
2024-10-31 14:16:45 +0100

    create content container devspace and vhat and thread

--- src/app/main-content/chat/chat.component.ts
+++ src/app/main-content/chat/chat.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ChatHeaderComponent } from "./chat-header/chat-header.component";
+import { ChatMessageFieldComponent } from "./chat-message-field/chat-message-field.component";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ChatHeaderComponent, ChatMessageFieldComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
