8db794e50d97e67ac387e3506154667fb5786b1e
EnsslinAdrian <ensslin.adrian@web.de>
2024-10-31 13:59:35 +0100

    create devspace adn chat and thread container

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MainHeaderComponent } from '../shared/header/main-header.component';
+import { DevspaceComponent } from "./devspace/devspace.component";
+import { ChatComponent } from "./chat/chat.component";
+import { ThreadComponent } from "./thread/thread.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [ MainHeaderComponent],
+  imports: [MainHeaderComponent, DevspaceComponent, ChatComponent, ThreadComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
