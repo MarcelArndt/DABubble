2566a6555747234d1d6bb3a43613719e91dda96d
EnsslinAdrian <ensslin.adrian@web.de>
2024-10-31 16:03:14 +0100

    create memeber message and my message component

--- src/app/main-content/chat/chat.component.ts
+++ src/app/main-content/chat/chat.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ChatHeaderComponent } from "./chat-header/chat-header.component";
 import { ChatMessageFieldComponent } from "./chat-message-field/chat-message-field.component";
+import { MemberMessageComponent } from "./member-message/member-message.component";
+import { MyMessageComponent } from "./my-message/my-message.component";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [ChatHeaderComponent, ChatMessageFieldComponent],
+  imports: [ChatHeaderComponent, ChatMessageFieldComponent, MemberMessageComponent, MyMessageComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
