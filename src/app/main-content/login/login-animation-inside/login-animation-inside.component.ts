e184c9f2880cc1cd664ee504bee8998fbead3943
MarcelArndt <marcel.arndt@hotmail.com>
2024-11-02 13:50:29 +0100

    animation of logo part 2

---
+++ src/app/main-content/login/login-animation-inside/login-animation-inside.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-animation-inside',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-animation-inside.component.html',
+  styleUrl: './login-animation-inside.component.scss'
+})
+export class LoginAnimationInsideComponent {
+
+}
--- src/app/main-content/login/login.component.ts
+++ src/app/main-content/login/login.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { LoginAnimationComponent } from './login-animation/login-animation.component';
+import { LoginAnimationInsideComponent } from './login-animation-inside/login-animation-inside.component';
 import { RouterModule } from '@angular/router';
 import { MainContentComponent } from '../main-content.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginAnimationComponent, RouterModule, MainContentComponent ],
+  imports: [LoginAnimationComponent, LoginAnimationInsideComponent, RouterModule, MainContentComponent ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss', './animation.inside.login.scss']
 })
 export class LoginComponent {
 
