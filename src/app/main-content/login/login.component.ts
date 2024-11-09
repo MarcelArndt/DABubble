3101e4e1f7ead6ed03e8013bfa56e373d0627463
MarcelArndt <marcel.arndt@hotmail.com>
2024-11-09 12:21:14 +0100

    fixing icons

--- src/app/main-content/login/login.component.ts
+++ src/app/main-content/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { LoginAnimationComponent } from './login-animation/login-animation.component';
 import { LoginAnimationInsideComponent } from './login-animation-inside/login-animation-inside.component';
 import { RouterModule } from '@angular/router';
-import { MainContentComponent } from '../main-content.component';
 import { HeadmenuLoginComponent } from './headmenu-login/headmenu-login.component';
 import { SubmenuLoginComponent } from './submenu-login/submenu-login.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -14,7 +13,7 @@ import { SuccessCreatingAccountComponent } from './success-creating-account/succ
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginAnimationComponent, CommonModule, LoginAnimationInsideComponent, RouterModule, MainContentComponent, HeadmenuLoginComponent, SubmenuLoginComponent, SignUpComponent, SignInComponent, ChooseAvatarComponent, SuccessCreatingAccountComponent],
+  imports: [LoginAnimationComponent, CommonModule, LoginAnimationInsideComponent, RouterModule, HeadmenuLoginComponent, SubmenuLoginComponent, SignUpComponent, SignInComponent, ChooseAvatarComponent, SuccessCreatingAccountComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss',]
 })
@@ -30,6 +29,7 @@ export class LoginComponent {
       this.pageNumber = index;
       this.checkforStepDirection();
     }  
+
   }
 
   checkforStepDirection(){
@@ -38,7 +38,6 @@ export class LoginComponent {
 
   getClass(index:number = 0){
     let newClass = 'still-deactive';
-   
       if(index == this.pageNumber){
         newClass = this.isStepForwards ? 'active-forwards' : 'active-backwards';
       }
@@ -48,7 +47,9 @@ export class LoginComponent {
       if(index + 1 == this.pageNumber && this.isStepForwards){
         newClass = 'deactive-forwards';
       }
-    
+      if(this.pageNumberTrashHolder - 2 > this.pageNumber && index != this.pageNumber){
+        newClass = 'still-deactive';
+      }
     return newClass;
 }
 
