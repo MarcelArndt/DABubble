cb9694b54e56efe506334f536da850c9871e2d34
EnsslinAdrian <ensslin.adrian@web.de>
2024-11-17 12:07:08 +0100

    chnage storage in auth

--- src/services/authentication/authentication.service.ts
+++ src/services/authentication/authentication.service.ts
@@ -352,42 +352,6 @@ export class AuthenticationService {
     } 
   }
 
-
-
-  // cloud storage 
-  uploadMultipleImages(files: FileList, folderName: string = 'User') {
-    Array.from(files).forEach((file, index) => {
-      const fileRef = ref(this.storage, `${folderName}/${this.getCurrentUserUid()}/${file.name}`);
-      console.log('herllo')
-      uploadBytes(fileRef, file).then((snapshot) => {
-        console.log(`Datei ${index + 1} hochgeladen: ${file.name}`);
-      }).catch(error => {
-        console.error(`Fehler beim Hochladen der Datei ${file.name}:`, error);
-      });
-    });
-  }
-
-  async getDownloadURLFromFirebase(file: File, folderName: string = 'User') {
-    const fileRef = ref(this.storage, `${folderName}/${this.getCurrentUserUid()}/${file.name}`);
-    return getDownloadURL(fileRef);
-  }
-
-  async uploadImage(file: File, folderName: string = 'User'): Promise<string> {
-    const fileRef = ref(this.storage, `${folderName}/${this.getCurrentUserUid()}/${file.name}`);
-
-    return uploadBytes(fileRef, file)
-      .then(() => {
-        console.log('File uploaded:', file);
-        // Abrufen der Download-URL nach erfolgreichem Upload
-        return getDownloadURL(fileRef);
-      })
-      .catch(error => {
-        console.error('Error uploading file:', error);
-        throw error; // Weitergeben des Fehlers zur Fehlerbehandlung
-      });
-  }
-
-
   // Lost Password
   async resetPassword(email:string){
     sendPasswordResetEmail(this.auth, email)
