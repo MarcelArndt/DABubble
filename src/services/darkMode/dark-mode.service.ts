00b9d03754405296ed3146efc2110c24c135b066
EnsslinAdrian <ensslin.adrian@web.de>
2024-11-25 18:01:09 +0100

    dark mode

--- src/services/darkMode/dark-mode.service.ts
+++ src/services/darkMode/dark-mode.service.ts
@@ -4,14 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DarkModeService {
-  public darkMode = true;
+  public darkMode = false;
 
   constructor() {
+    this.loadDarkModeFromStorage();
     this.applyDarkMode();
   }
 
   toggleDarkMode(): void {
     this.darkMode = !this.darkMode;
+    this.saveDarkModeToStorage();
     this.applyDarkMode();
   }
 
@@ -25,5 +27,14 @@ export class DarkModeService {
     } else {
       document.documentElement.classList.remove('dark-theme');
     }
+  }
+
+  private saveDarkModeToStorage(): void {
+    localStorage.setItem('darkMode', JSON.stringify(this.darkMode));
+  }
+
+  private loadDarkModeFromStorage(): void {
+    const storedValue = localStorage.getItem('darkMode');
+    this.darkMode = storedValue ? JSON.parse(storedValue) : false;
   }
 }
