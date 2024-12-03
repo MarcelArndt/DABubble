beaa499a8cf6daf1ae7d65f10e80dd4ad3e07108
Michael Krämer <info.m.kraemer@gmail.com>
2024-12-03 16:52:30 +0100

    lazy loading for devspace, thread, chat und main header in main-content; css nano and css purify integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,15 @@
 import { Routes } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
 import { LoginComponent } from './main-content/login/login.component';
-import { ThreadComponent } from './main-content/thread/thread.component';
-import { ChatComponent } from './main-content/chat/chat.component';
-import { AuthGuard } from '../guard/auth.guard';
 import { ResetPasswordComponent } from './main-content/login/reset-password/reset-password.component';
 import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'lost-password', component: ResetPasswordComponent}, 
+  { path: 'lost-password', component: ResetPasswordComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'imprint', component: ImprintComponent},
+  { path: 'imprint', component: ImprintComponent },
   {
     path: 'start',
-    component: MainContentComponent,
-    children: [
-        {
-            path: 'start/channels/:channelId',
-            component: ChatComponent,
-            canActivate: [AuthGuard] 
-          },
-          {
-            path: 'start/channels/:channelId/threads/:threadId',
-            component: ThreadComponent,
-            canActivate: [AuthGuard]
-          },
-          
-      { path: '', redirectTo: 'channels/welcome', pathMatch: 'full' },
-    ]
-  },
+    loadChildren: () => import('./main-content/main-content-routing.module').then(m => m.MainContentRoutingModule)
+  }
 ];
