2a1cb095fff35b46d8eb7cb0ca2ef1dc16aad0d4
Michael Krämer <info.m.kraemer@gmail.com>
2024-12-06 15:46:50 +0100

    performance; devspace; folder split components; searchbar - dropdown menue of searchbar

--- src/app/main-content/devspace/devspace.component.ts
+++ src/app/main-content/devspace/devspace.component.ts
@@ -1,21 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, inject, Input, OnInit, Renderer2} from '@angular/core';
+import { Component, inject, OnInit} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { trigger, style, animate, transition, query } from '@angular/animations';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { CreateChannelComponent } from '../../dialog/create-channel/create-channel.component';
-import { Member, Message } from '../../../interface/message';
+import { Member } from '../../../interface/message';
 import { MemberService } from '../../../services/member/member.service';
 import { ChannelService } from '../../../services/channel/channel.service';
 import { Channel } from '../../../classes/channel.class';
 import { MainContentService } from '../../../services/main-content/main-content.service';
 import { AuthenticationService } from '../../../services/authentication/authentication.service';
-import { SearchbarComponent } from '../../shared/header/searchbar/searchbar.component';
 import { MessagesService } from '../../../services/messages/messages.service';
 import { DirectMessageService } from '../../../services/directMessage/direct-message.service';
-import { BehaviorSubject, combineLatest, debounceTime, distinctUntilChanged, filter, firstValueFrom, Observable, of, switchMap } from 'rxjs';
+import { combineLatest } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { SearchBarComponentDevSpace } from './search-bar/search-bar.component';
 
 
 @Component({
@@ -26,7 +26,8 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     MatIcon,
     MatDialogModule,
-    FormsModule
+    FormsModule,
+    SearchBarComponentDevSpace
   ],
   templateUrl: './devspace.component.html',
   styleUrl: './devspace.component.scss',
@@ -43,7 +44,6 @@ import { FormsModule } from '@angular/forms';
     trigger('toggleNavBar', [
       transition(':enter', [
         style({
-          // width: '0px',
           opacity: 0,
           overflow: 'hidden',
           transform: 'translateX(-100%)'
@@ -51,7 +51,6 @@ import { FormsModule } from '@angular/forms';
         animate(
           '125ms ease-out',
           style({
-            // width: '302.33px', 
             opacity: 1,
             transform: 'translateX(0)'
           })
@@ -69,7 +68,6 @@ import { FormsModule } from '@angular/forms';
         animate(
           '125ms ease-in',
           style({
-            // width: '0px', 
             opacity: 0,
             overflow: 'hidden',
             transform: 'translateX(-100%)'
@@ -80,58 +78,14 @@ import { FormsModule } from '@angular/forms';
   ]
 })
 export class DevspaceComponent implements OnInit {
-  searchQuery = ''; 
-  showDropdown = false;
-  displayHints = false;
-  activeDropdownIndex = -1; // Aktives Element im Dropdown
-  previousSearchChannel: Channel | null = null;
-  searchChanges$: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  allHints = [
-    "Type '@' for members.",
-    "Type '#' for channels.",
-    "Select a Channel to search for messages."
-  ];
-
   contactsAreVisible: boolean = true;
   channelsAreVisible: boolean = true;
   readonly dialog = inject(MatDialog);
 
   members: Member[] = [];
   channels: Channel[] = [];
-  messages: Message[] = [];
   currentMember?: Member | null = null;
   currentMember$;
-
-  searchbarChannel: Channel[] = [];
-  searchbarMember: Member[] = [];
-
-
-  //Searchbar
-  @Input() icon: string = 'search';
-  @Input() addClass: string[] = [];
-  @Input() placeholder: string = 'Search in Devspace';
-  @Input() align_reverse: boolean = false;
-
-  @HostListener('focusin', ['$event'])
-  onFocusIn(event: Event): void {
-    const parent = this.elRef.nativeElement.querySelector('.searchbar');
-    if (parent) {
-      this.renderer.addClass(parent, 'active');
-    } else {
-      console.warn('Element .searchbar nicht gefunden');
-    }
-  }
-
-  @HostListener('focusout', ['$event'])
-  onFocusOut(event: Event): void {
-    const parent = this.elRef.nativeElement.querySelector('.searchbar');
-    if (parent) {
-      this.renderer.removeClass(parent, 'active');
-    } else {
-      console.warn('Element .searchbar nicht gefunden');
-    }
-  }
-  ///////
 
 
   constructor(
@@ -140,9 +94,7 @@ export class DevspaceComponent implements OnInit {
     private channelService: ChannelService,
     public mainContentService: MainContentService,
     private directMessageService: DirectMessageService,
-    private authenticationService: AuthenticationService,
-    private renderer: Renderer2,
-    private elRef: ElementRef) {
+    private authenticationService: AuthenticationService) {
       this.currentMember$ = this.authenticationService.currentMember$;
   }
 
@@ -151,220 +103,8 @@ export class DevspaceComponent implements OnInit {
     this.initializeMemberAndChannels(); 
     this.initializePublicChannels(); 
     this.authenticationService.observerUser();
-    this.initializeSearchListeners();
-  }
-
-  ngAfterViewInit() {
-    // Zugriff auf die Dropdown-Elemente nach dem View-Init
-    this.addGlobalClickEventListener();
   }
   
-  addGlobalClickEventListener() {
-    document.addEventListener('click', (event: MouseEvent) => {
-      const inputElement = this.elRef.nativeElement.querySelector('input');
-      const dropdownElement = this.elRef.nativeElement.querySelector('.dropdown');
-      const clickedInsideInput = inputElement?.contains(event.target as Node);
-      const clickedInsideDropdown = dropdownElement?.contains(event.target as Node);
-      if (!clickedInsideInput && !clickedInsideDropdown) {
-        this.hideDropdown();
-      }
-    });
-  }
-
-    
-  onDropdownItemClick(index: number) {
-    this.activeDropdownIndex = index;
-    this.selectDropdownItem(); 
-  }
-
-  
-  initializeSearchListeners() {
-    this.authenticationService.currentMember$.pipe(
-      filter(currentMember => !!currentMember), // Nur fortfahren, wenn der Benutzer verfügbar ist
-      switchMap(currentMember => {
-        if (!currentMember) {
-          console.error('No current user is signed in.');
-          return of([]); // Rückgabe von leeren Ergebnissen, wenn der Benutzer nicht vorhanden ist
-        }
-        // Wenn der Benutzer verfügbar ist, lade Mitglieder und Kanäle
-        const members$ = this.memberService.getAllMembersFromFirestoreObservable();
-        const channels$ = this.channelService.getAllAccessableChannelsFromFirestoreObservable(currentMember);
-
-        return this.searchChanges$.pipe(
-          debounceTime(300), 
-          distinctUntilChanged(),
-          switchMap(query => this.processSearchQuery(query, members$, channels$))
-        );
-      })
-    ).subscribe(() => {
-      this.showDropdown = this.searchbarMember.length > 0 || this.searchbarChannel.length > 0 || this.messages.length > 0;
-    });
-  }
-
-
-  onSearchInput(query: string) {
-    this.searchQuery = query.trim();
-    this.searchChanges$.next(this.searchQuery);
-    this.displayHints = !this.searchQuery.trim(); 
-    this.toggleHintsBasedOnInput(query);
-  }
-  
-
-  toggleHintsBasedOnInput(query: string) {
-    if (query === '') {
-      this.messageService.readChannel();
-      this.messageService.isSearchForMessages = false;
-      this.displayHints = true;
-    } else {
-      // Wenn das Inputfeld nicht leer ist, blende die Hinweise aus
-      this.displayHints = false;
-    }
-  }
-  
-  
-async processSearchQuery(
-  query: string, 
-  members$: Observable<Member[]>, 
-  channels$: Observable<Channel[]>
-) {
-  this.searchbarMember = [];
-  this.searchbarChannel = [];
-  this.messages = [];
-
-  if (query.startsWith('@')) {
-    // Suche nach Mitgliedern
-    const members = await firstValueFrom(members$);
-    this.searchbarMember = members.filter(member => 
-    member.name.toLowerCase().includes(query.slice(1).toLowerCase())
-    );
-  }  else if (query.startsWith('#')) {
-    // Suche nach Channels
-    const channels = await firstValueFrom(channels$);
-    this.searchbarChannel = channels.filter(channel => 
-      channel.title.toLowerCase().includes(query.slice(1).toLowerCase()) 
-      // && (!this.previousSearchChannel || channel.id !== this.previousSearchChannel.id) // Aktuellen Channel ausschließen
-    );
-  }
-  if (this.previousSearchChannel && query.includes(' ')) {
-    const channels = await firstValueFrom(channels$);
-    this.searchbarChannel = channels.filter(channel => 
-      channel.title.toLowerCase().includes(query.slice(1).toLowerCase()) 
-      && (!this.previousSearchChannel || channel.id !== this.previousSearchChannel.id) // Aktuellen Channel ausschließen
-    );
-    const channelTitle = this.previousSearchChannel.title.toLowerCase(); 
-    const searchQuery = query.toLowerCase().replace(`#${channelTitle}`, '').trim(); 
-    const allMessages = await this.messageService.loadInitialMessagesByChannelId(this.previousSearchChannel.id);
-    this.messages = allMessages.filter((message: Message) => 
-      message.message.toLowerCase().includes(searchQuery)
-    );
-    this.messageService.isSearchForMessages = true;
-    this.messageService.messages = this.messages;
-    this.messageService.searchQuery = searchQuery; 
-    this.messageService.messagesUpdated.next();
-  }
-}
-
-
-  showHints() {
-    if (!this.searchQuery.trim()) {
-      this.searchbarMember = [];
-      this.searchbarChannel = [];
-      this.messages = [];
-      this.displayHints = true;
-      this.showDropdown = true;
-    }
-  }
-
-  hideDropdown() {
-    setTimeout(() => {
-      this.displayHints = false;
-      this.showDropdown = false;
-      this.activeDropdownIndex = -1;
-    }, 200);
-  }
-
-  navigateDropdown(direction: number) {
-    if (!this.showDropdown && !this.displayHints) return;
-    const totalResults = this.displayHints ? this.allHints.length : this.searchbarMember.length + this.searchbarChannel.length + this.messages.length;
-    this.activeDropdownIndex = (this.activeDropdownIndex + direction + totalResults) % totalResults;
-    this.setActiveDropdownIndex(this.activeDropdownIndex);
-  }
-  
-
-  selectDropdownItem() {
-    if (this.activeDropdownIndex < 0) return;
-    // Wenn der aktive Index in den Hinweisen liegt
-    if ((this.activeDropdownIndex < this.allHints.length) && this.displayHints) {
-      const hint = this.allHints[this.activeDropdownIndex];
-      if (hint.includes('@')) {
-        this.searchQuery = '@';  // Setzt das @ im Suchfeld
-        this.onSearchInput(this.searchQuery);
-        this.searchbarMember = []; // Lade Mitglieder
-        this.showDropdown = true;
-      } else if (hint.includes('#')) {
-        this.searchQuery = '#';  // Setzt das # im Suchfeld
-        this.onSearchInput(this.searchQuery);
-        this.searchbarChannel = []; // Lade Kanäle
-        this.showDropdown = true;
-      }
-      this.displayHints = false;  // Blende die Hinweise aus
-    } 
-    else if ( !this.displayHints) {
-      // Wenn der aktive Index innerhalb der Mitglieder oder Kanäle ist, wähle diese aus
-      let selectedItem: Member | Channel | Message | null = null;
-      let itemType: string = '';
-      if (this.activeDropdownIndex < this.searchbarMember.length) {
-        selectedItem = this.searchbarMember[this.activeDropdownIndex];
-        itemType = 'member';
-      } else if (this.activeDropdownIndex < this.searchbarChannel.length) {
-        selectedItem = this.searchbarChannel[this.activeDropdownIndex];
-        itemType = 'channel';
-      } else if (this.activeDropdownIndex < this.messages.length) {
-        selectedItem = this.messages[this.activeDropdownIndex];
-        itemType = 'message';
-      }
-      if (selectedItem) {
-        this.handleSelectItem(selectedItem, itemType);
-        this.activeDropdownIndex = -1;
-      }
-    }
-  }
-  
-
-  async handleSelectItem(selectedItem: Member | Channel | Message, itemType: string) {
-    if (itemType === 'channel') {
-      // Kanal auswählen
-      this.previousSearchChannel = selectedItem as Channel;
-      this.searchQuery = `#${(selectedItem as Channel).title}`;
-      this.channelService.currentChannelId = (selectedItem as Channel).id;
-      this.messageService.readChannel(); 
-    } else if (itemType === 'member') {
-      // Mitglied auswählen
-      this.searchQuery = `@${(selectedItem as Member).name}`;
-      this.memberService.openProfileUser((selectedItem as Member).id);
-    } if (itemType === 'message') {
-      const selectedMessage = selectedItem as Message;
-      this.searchQuery = `#${(this.previousSearchChannel as Channel).title} ${selectedMessage.message}`; 
-      this.onSearchInput(this.searchQuery);
-    }
-  }
-  
-
-  setActiveDropdownIndex(index: number) {
-    this.activeDropdownIndex = index;
-    const dropdownElement = this.elRef.nativeElement.querySelector('.dropdown');
-    const allElements = dropdownElement?.querySelectorAll('.dropDownItem') || []; 
-    const activeElement = allElements[index] as HTMLElement;
-    if (activeElement) {
-      activeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-    }
-  }
-
-  closeDevSpaceAndOpenChatForMobile(){
-    this.mainContentService.closeNavBar();
-    this.mainContentService.makeChatAsTopLayer();
-  }
-
 
   initializeMemberAndChannels(): void {
     const members$ = this.memberService.getAllMembersFromFirestoreObservable();
