14c4d952a59f04f003f8639a06db2ee4d9e3c084
Michael Krämer <info.m.kraemer@gmail.com>
2024-12-08 12:54:04 +0100

    Removed performance tools; lazy loading, purify css and css nano

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './main-content/login/login.component';
 import { ResetPasswordComponent } from './main-content/login/reset-password/reset-password.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'lost-password', component: ResetPasswordComponent },
   { path: 'login', component: LoginComponent },
   { path: 'imprint', component: ImprintComponent },
-  {
-    path: 'start',
-    loadChildren: () => import('./main-content/main-content-routing.module').then(m => m.MainContentRoutingModule)
-  }
+  { path: 'start', component: MainContentComponent }
 ];
