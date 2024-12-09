c1d3ea9896d5c3094b71d369ef91afaba5985a1e
MarcelArndt <marcel.arndt@hotmail.com>
2024-12-09 19:17:57 +0100

    some routers fixing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './main-content/login/login.component';
 import { ResetPasswordComponent } from './main-content/login/reset-password/reset-password.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { LostPasswordComponent } from './main-content/login/lost-password/lost-password.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'lost-password', component: ResetPasswordComponent },
+  { path: 'lost-password', component: LostPasswordComponent },
   { path: 'login', component: LoginComponent },
   { path: 'imprint', component: ImprintComponent },
-  { path: 'start', component: MainContentComponent }
+  { path: 'start', component: MainContentComponent },
+  { path: 'auth/action', component: ResetPasswordComponent }
 ];
